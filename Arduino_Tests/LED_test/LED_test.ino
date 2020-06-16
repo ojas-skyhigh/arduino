f9239c7391d7698b63ec5e3308fa1f1b70928588
ojas-skyhigh <65460684+ojas-skyhigh@users.noreply.github.com>
2020-06-16 22:05:35 +0530

    Update LED_test.ino

--- Arduino_Tests/LED_test/LED_test.ino
+++ Arduino_Tests/LED_test/LED_test.ino
@@ -1,12 +1,21 @@
-int led_pin = A1;
+
+int led_pin = 2;
 
 void setup() {
-  pinMode(led_pin,OUTPUT);// put your setup code here, to run once:
+  // put your setup code here, to run once:
+  pinMode(led_pin, OUTPUT); // Sets pin to either input or output . In our case the LED is an output as it needs to produce light.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-analogWrite(led_pin,1);
-delay(2000);
-analogWrite(led_pin,LOW);
+  digitalWrite(led_pin, HIGH);// Used to assign power to the pin which is now "HIGH" which gives power ,switching the LED on.
+  delay(2000); // Waits for 2 seconds.
+  digitalWrite(led_pin, LOW);  // Used to assign power to the pin which is now "LOW" which stops power ,switching the LED off.
+  delay(2000); // Waits for 2 seconds.
 }
+
+/* Function explanation :
+  pinMode is used to set pin to either input or output . In our case the LED is an output as it needs to produce light.
+  digitalWrite is used to assign a HIGH or LOW value which means On and Off value respectively.
+  delay is used to tell the Arduino to wait and continue the last thing it was doing for the assigned time.
+  
